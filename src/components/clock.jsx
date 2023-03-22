4f8408ad2f03493901afd1f877fc38028f7399c5
Twist_DEV <alexandre.durand4444@gmail.com>
2023-03-22 09:15:32 +0100

    Ajout d'une card et affichage des restaurants

---
+++ src/components/clock.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function Clock (){
+   //Initialisation de l'etat local
+   const [date, setDate] = useState(new Date())
+//Fonction appelée automatiquement au montage de composant 
+   useEffect (()=> {
+      //Fonction appelée tous les 100ms
+      const interval =setInterval(()=>{
+         setDate(new Date())
+      },1000)
+   }, [])
+//Afichage de composant
+   return(
+      <h1>{date.toLocaleTimeString()}</h1>
+   )
+}
+
+export default Clock
---
+++ src/components/header.jsx
@@ -0,0 +1,25 @@
+import '../Styles/headerStyle.css'
+
+function Header() {
+   return ( 
+      <header>
+         <div className="header">
+            <div className="header-menu">
+               <a href="#">Menu 1</a>
+               <a href="#">Menu 2</a>
+               <a href="#">Menu 3</a>
+            </div>
+            <div className="header-logo">
+               <img src="https://cdn.worldvectorlogo.com/logos/50x50.svg" alt="Logo" />
+            </div>
+            <div className="header-reseaux-sociaux">
+               <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/25px-LinkedIn_icon.svg.png" alt="LinkedIn" /></a>
+               <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/25px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" /></a>
+               <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/25px-Instagram_icon.png" alt="Instagram" /></a>
+            </div>
+         </div>
+      </header>
+    )
+}
+
+export default Header;
